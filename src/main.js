8a95693431a4b6e6d08a047e9779aa8a73f9e649
Pascal <pascal_buehler@sluz.ch>
2025-03-24 13:51:05 +0100

    i18n heruntergeladen und herumexperimentiert

--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import de from './locales/de.json'
+import it from './locales/it.json'
+import sp from './locales/sp.json'
+import zh from './locales/zh.json'
+
+const i18n = createI18n({
+    locale: 'en', // Standardsprache
+    fallbackLocale: 'en', // Fallback, falls Übersetzung fehlt
+    messages: { en, de, sp, zh, it }
+  });
 
 const app = createApp(App)
-
+app.use(i18n);
 app.use(router)
 
 app.mount('#app')
