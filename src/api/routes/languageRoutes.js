960840b420528eb5fa309f1e34997f175d683c06
julianamschwand <julian_amschwand@sluz.ch>
2025-04-09 12:55:06 +0200

    Filled in the language route

--- src/api/routes/languageRoutes.js
+++ src/api/routes/languageRoutes.js
@@ -1,1 +1,10 @@
 import api from "../api.js"
+
+export async function languages() {
+	try {
+		const res = await api.get("/getlanguages")
+		return res.data
+	} catch (error) {
+		console.error("Error:", error)
+	}
+}
