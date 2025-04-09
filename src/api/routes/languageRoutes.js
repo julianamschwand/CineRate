31e736e33d3d0b7d8215560bfe69ffa93fce069f
julianamschwand <julian_amschwand@sluz.ch>
2025-04-09 12:58:48 +0200

    Filled in rating routes

--- src/api/routes/languageRoutes.js
+++ src/api/routes/languageRoutes.js
@@ -1,6 +1,6 @@
 import api from "../api.js"
 
-export async function languages() {
+export async function getlanguages() {
 	try {
 		const res = await api.get("/getlanguages")
 		return res.data
--- src/api/routes/ratingRoutes.js
+++ src/api/routes/ratingRoutes.js
@@ -1,1 +1,24 @@
 import api from "../api.js"
+
+export async function getrating(movieid) {
+	try {
+		const res = await api.get("/getrating", {
+      movieid
+		})
+		return res.data
+	} catch (error) {
+		console.error("Error:", error)
+	}
+}
+
+export async function rate(movieid, rating) {
+	try {
+		const res = await api.get("/rate", {
+      movieid,
+      rating
+		})
+		return res.data
+	} catch (error) {
+		console.error("Error:", error)
+	}
+}
